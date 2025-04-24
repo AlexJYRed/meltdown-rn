b6cfe75d7d7bc82c0e77ddaa42fad2a5de220dca
AlexJYRed <alexjyred@gmail.com>
2025-04-23 22:54:28 -0700

    rearranging

--- frontend/app/game.js
+++ frontend/app/game.js
@@ -111,10 +111,7 @@ export default function GameScreen() {
         </View>
 
         <View style={styles.controls}>
-          <Text style={{ color: "black" }}>myId: {myId}</Text>
-          <Text style={{ color: "black" }}>
-            my state: {JSON.stringify(allStates[myId]?.state)}
-          </Text>
+         
 
           {rule ? (
             <Text style={styles.smalltext}>
@@ -144,6 +141,11 @@ export default function GameScreen() {
 
         <View style={{ padding: 40 }}>
           {/* <Text style={{ fontSize: 24, marginBottom: 20 }}>Control Room</Text> */}
+
+          <Text style={{ color: "black" }}>myId: {myId}</Text>
+          <Text style={{ color: "black" }}>
+            my state: {JSON.stringify(allStates[myId]?.state)}
+          </Text>
 
           <Text style={{ marginTop: 30, fontSize: 16 }}>Players:</Text>
           {Object.entries(allStates).map(([id, player]) => (
